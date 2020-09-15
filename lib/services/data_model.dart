b806d27e5d4e647acb25baf4699be34f142428b6
HHK <hhk.01831@gmail.com>
2020-09-15 18:52:54 +0300

    minor code debug

--- lib/services/data_model.dart
+++ lib/services/data_model.dart
@@ -9,13 +9,30 @@ class UserData {
   final String uid;
   final String name;
   final String speciality;
+  final String phoneNumber;
+  final String province;
+  final String location;
 
-  UserData({this.uid, this.name, this.speciality});
+  UserData(
+      {this.uid,
+      this.name,
+      this.speciality,
+      this.phoneNumber,
+      this.province,
+      this.location});
 }
 
 class Info {
   final String name;
   final String speciality;
+  final String phoneNumber;
+  final String province;
+  final String location;
 
-  Info({this.name, this.speciality});
+  Info(
+      {this.name,
+      this.speciality,
+      this.phoneNumber,
+      this.province,
+      this.location});
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:project_doctor/services/data_model.dart';
+import 'package:project_doctor/services/data_model.dart';
 
 class DatabaseService {
   final String uid;
@@ -14,29 +14,29 @@ class DatabaseService {
         .set({'name': name, 'speciality': speciality});
   }
 
-  // // info list from a snapshot
-  // List<Info> _infoListFromSnapshot(QuerySnapshot snapshot) {
-  //   return snapshot.docs.map((doc) {
-  //     return Info(
-  //         name: doc.data()['name'] ?? '',
-  //         speciality: doc.data()['speciality'] ?? '');
-  //   }).toList();
-  // }
+  // info list from a snapshot
+  List<Info> _infoListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Info(
+          name: doc.data()['name'] ?? '',
+          speciality: doc.data()['speciality'] ?? '');
+    }).toList();
+  }
 
-  // // user data from snapshot
-  // UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
-  //   return UserData(
-  //     uid: uid,
-  //     name: snapshot.data()['name'],
-  //     speciality: snapshot.data()['speciality'],
-  //   );
-  // }
+  // user data from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data()['name'],
+      speciality: snapshot.data()['speciality'],
+    );
+  }
 
-  // Stream<List<Info>> get info {
-  //   return userCollection.snapshots().map(_infoListFromSnapshot);
-  // }
+  Stream<List<Info>> get info {
+    return userCollection.snapshots().map(_infoListFromSnapshot);
+  }
 
-//   Stream<UserData> get userDate {
-//     return userCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
-//   }
+  Stream<UserData> get userDate {
+    return userCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
+  }
 }
