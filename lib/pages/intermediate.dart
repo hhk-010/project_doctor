4a5dabdc4f5d020d3d010dcc6cdec2bd1be363ba
HHK <hhk.01831@gmail.com>
2020-09-16 01:58:20 +0300

    --

--- lib/pages/intermediate.dart
+++ lib/pages/intermediate.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:project_doctor/services/data_model.dart';
 import 'package:project_doctor/services/auth.dart';
 
+//intermediate page created to solve the prolbem of the streamprovider.
 class Intermediate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
