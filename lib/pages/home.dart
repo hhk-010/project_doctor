bc0763cdc2c1f7b1e396b52d46b3e665737838fd
HHK <hhk.01831@gmail.com>
2020-12-01 22:51:49 +0300

    Responsive Design 25%

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,158 +24,173 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     var lang = Localizations.localeOf(context).languageCode;
     return ResponsiveBuilder(builder: (context, sizingInformation) {
+      double appBar;
+      double appBarIcon;
+      double appBarHeight;
+      double buttonHeight;
+      double buttonWidth;
+      double buttonTitle;
+      double buttonSubTitle;
+      double buttonIconSize;
 
-
-      // double buttonHeight =  sizingInformation.deviceScreenType == DeviceScreenType.Mobile ? : ;
-      // double buttonWidth =  sizingInformation.deviceScreenType == DeviceScreenType.Tablet ? : ;
-
-
-
-
-
-
-
-
-
-
-
+      if (sizingInformation.deviceScreenType == DeviceScreenType.Mobile) {
+        appBar = displayHeight(context) * 0.03;
+        appBarIcon = 25;
+        appBarHeight = 60;
+        buttonHeight = displayHeight(context) * 0.13;
+        buttonWidth = displayWidth(context) * 0.85;
+        buttonTitle = displayHeight(context) * 0.035;
+        buttonSubTitle = displayHeight(context) * 0.022;
+        buttonIconSize = 70;
+      } else {
+        appBar = displayHeight(context) * 0.045;
+        appBarIcon = 40;
+        appBarHeight = 75;
+        buttonHeight = displayHeight(context) * 0.15;
+        buttonWidth = displayWidth(context) * 0.6;
+        buttonTitle = displayHeight(context) * 0.045;
+        buttonSubTitle = displayHeight(context) * 0.03;
+        buttonIconSize = 100;
+      }
 
       return Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.info),
-            tooltip: AppLocalizations.of(context).translate('about_us'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/about_us');
-            },
-          ),
-          title: FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Text(
-              AppLocalizations.of(context).translate('home_title'),
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(appBarHeight),
+          child: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.info),
+              tooltip: AppLocalizations.of(context).translate('about_us'),
+              iconSize: appBarIcon,
+              onPressed: () {
+                Navigator.pushNamed(context, '/about_us');
+              },
             ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrange,
-          elevation: 0,
-          actions: [
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(0, 16, 16, 14),
-                child: Tooltip(
-                  message: AppLocalizations.of(context).translate('language'),
-                  child: DropdownButton(
-                    onChanged: (Language language) {
-                      _changeLanguage(language);
-                    },
-                    underline: SizedBox(),
-                    icon: Icon(
-                      Icons.language,
-                      color: Colors.white,
+            title: FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Text(
+                AppLocalizations.of(context).translate('home_title'),
+                style: TextStyle(fontSize: appBar, fontWeight: FontWeight.bold),
+              ),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.deepOrange,
+            elevation: 0,
+            actions: [
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 16, 16, 14),
+                  child: Tooltip(
+                    message: AppLocalizations.of(context).translate('language'),
+                    child: DropdownButton(
+                      onChanged: (Language language) {
+                        _changeLanguage(language);
+                      },
+                      underline: SizedBox(),
+                      icon: Icon(
+                        Icons.language,
+                        size: appBarIcon,
+                        color: Colors.white,
+                      ),
+                      items: Language.languageList(context)
+                          .map<DropdownMenuItem<Language>>(
+                            (lang) => DropdownMenuItem(
+                                value: lang,
+                                child: Text(
+                                  lang.name,
+                                )),
+                          )
+                          .toList(),
                     ),
-                    items: Language.languageList(context)
-                        .map<DropdownMenuItem<Language>>(
-                          (lang) => DropdownMenuItem(
-                              value: lang,
-                              child: Text(
-                                lang.name,
-                              )),
-                        )
-                        .toList(),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 75),
-            child: Container(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Container(
-                      height: displayHeight(context) * 0.15,
-                      width: displayWidth(context) * 0.9,
-                      child: RaisedButton.icon(
-                        onPressed: () => Navigator.pushNamed(context, '/patient_complain'),
-                        icon: Icon(
-                          Icons.search,
-                          size: 70,
-                          color: Colors.white,
-                        ),
-                        label: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: AutoSizeText.rich(
-                            TextSpan(
-                                text: AppLocalizations.of(context).translate('home_patient_title'),
-                                style: TextStyle(
-                                    fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
-                                    fontSize: 26,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
-                                children: [
-                                  TextSpan(
-                                      text: AppLocalizations.of(context).translate('home_patient_subtitle'),
-                                      style: TextStyle(
-                                          fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white)),
-                                ]),
-                            maxLines: 2,
-                          ),
-                        ),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-                        color: Colors.deepOrange,
+          child: Container(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    height: buttonHeight,
+                    width: buttonWidth,
+                    child: RaisedButton.icon(
+                      onPressed: () => Navigator.pushNamed(context, '/patient_complain'),
+                      icon: Icon(
+                        Icons.search,
+                        size: buttonIconSize,
+                        color: Colors.white,
                       ),
-                    ),
-                    SizedBox(
-                      height: 90,
-                      width: 300,
-                      child: RaisedButton.icon(
-                        onPressed: () => Navigator.pushNamed(context, '/intermediate'),
-                        icon: Icon(
-                          Icons.people,
-                          size: 70,
-                          color: Colors.white,
+                      label: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: AutoSizeText.rich(
+                          TextSpan(
+                              text: AppLocalizations.of(context).translate('home_patient_title'),
+                              style: TextStyle(
+                                  fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
+                                  fontSize: buttonTitle,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                              children: [
+                                TextSpan(
+                                    text: AppLocalizations.of(context).translate('home_patient_subtitle'),
+                                    style: TextStyle(
+                                        fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
+                                        fontSize: buttonSubTitle,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                              ]),
+                          maxLines: 2,
                         ),
-                        label: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: AutoSizeText.rich(
-                            TextSpan(
-                                text: AppLocalizations.of(context).translate('home_doctor_title'),
-                                style: TextStyle(
-                                    fontSize: 26,
-                                    fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
-                                children: [
-                                  TextSpan(
-                                      text: AppLocalizations.of(context).translate('home_doctor_subtitle'),
-                                      style: TextStyle(
-                                          fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white)),
-                                ]),
-                          ),
-                        ),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-                        color: Colors.deepOrange,
                       ),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                      color: Colors.deepOrange,
                     ),
-                  ],
-                ),
+                  ),
+                  Container(
+                    height: buttonHeight,
+                    width: buttonWidth,
+                    child: RaisedButton.icon(
+                      onPressed: () => Navigator.pushNamed(context, '/intermediate'),
+                      icon: Icon(
+                        Icons.people,
+                        size: buttonIconSize,
+                        color: Colors.white,
+                      ),
+                      label: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: AutoSizeText.rich(
+                          TextSpan(
+                              text: AppLocalizations.of(context).translate('home_doctor_title'),
+                              style: TextStyle(
+                                  fontSize: buttonTitle,
+                                  fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                              children: [
+                                TextSpan(
+                                    text: AppLocalizations.of(context).translate('home_doctor_subtitle'),
+                                    style: TextStyle(
+                                        fontFamily: lang == 'ar' ? 'noto_arabic' : 'Helvetica',
+                                        fontSize: buttonSubTitle,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                              ]),
+                        ),
+                      ),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
