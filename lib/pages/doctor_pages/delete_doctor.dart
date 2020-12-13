c6281099192008e01d17c3f016c84e7053783d3f
HHK <hhk.01831@gmail.com>
2020-12-13 12:08:16 +0300

    ----

--- lib/pages/doctor_pages/delete_doctor.dart
+++ lib/pages/doctor_pages/delete_doctor.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'dart:ui';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project_doctor/authorization/loading.dart';
 import 'package:project_doctor/constants/theme.dart';
