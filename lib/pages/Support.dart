69c3dcde8e4e687dd7d4a4aedc17e682732dabf9
HHK <hhk.01831@gmail.com>
2020-12-17 00:26:08 +0300

    delete image changes

--- lib/pages/Support.dart
+++ lib/pages/Support.dart
@@ -22,10 +22,11 @@ class _AboutUSState extends State<AboutUS> {
   }
 
   String aboutUs =
-      'Cura mobile app is developed by two certified medical practitioners trying to simplify the process of choosing the right medical specialty for the patient complaint';
-  String privacyPolicy = "";
+      "Cura mobile app is developed by two certified medical practitioners trying to approximate distance and simplifying  the process of choosing the right medical specialty for the patient complaint.";
+  String privacyPolicy1 = "";
+  String privacyPolicy2 = "";
   String termsConditions1 =
-      "By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages, or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to Cura Mobile.\n Cura team  is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.\n The Cura app stores and processes personal data that you have provided to us, in order to provide our Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Cura app won’t work properly or at all.\n You should be aware that there are certain things that Cura mobile will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi, or provided by your mobile network provider, but Cura mobile  cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.\n";
+      "By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages, or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to Cura Mobile.\n Cura team  is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.\n The Cura app stores and processes personal data that you have provided to us, in order to provide our Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Cura app won’t work properly or at all.\n You should be aware that there are certain things that Cura mobile will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi, or provided by your mobile network provider, but Cura mobile  cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.";
 
   String termsConditions2 =
       "If you’re using the app outside of an area with Wi-Fi, you should remember that your terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.\n Along the same lines, Cura cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, Cura mobile  cannot accept responsibility.\n With respect to Cura mobile responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavour to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. Cura mobile accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.\n At some point, we may wish to update the app. The app is currently available on Android & iOS – the requirements for both systems(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. Cura mobile does not promise that it will always update the app so that it is relevant to you and/or works with the Android & iOS version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.\n We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page.";
@@ -180,7 +181,18 @@ class _AboutUSState extends State<AboutUS> {
                                           child: Column(
                                         crossAxisAlignment: CrossAxisAlignment.stretch,
                                         children: [
-                                          Text(privacyPolicy, style: dialoge, textAlign: TextAlign.center),
+                                          Text(
+                                            privacyPolicy1,
+                                            style: dialoge,
+                                            textDirection: TextDirection.ltr,
+                                            textAlign: TextAlign.justify,
+                                          ),
+                                          Text(
+                                            privacyPolicy2,
+                                            style: dialoge,
+                                            textDirection: TextDirection.ltr,
+                                            textAlign: TextAlign.justify,
+                                          ),
                                         ],
                                       )),
                                       actions: <Widget>[
@@ -267,7 +279,14 @@ class _AboutUSState extends State<AboutUS> {
                             height: buttonHeight,
                             width: buttonWidth,
                             child: FlatButton(
-                              onPressed: () {},
+                              onPressed: () async {
+                                const url = 'https://sites.google.com/view/cura-mobile/home';
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could Not Launch $url';
+                                }
+                              },
                               child: Text(
                                 'Website',
                                 style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: title),
@@ -284,6 +303,9 @@ class _AboutUSState extends State<AboutUS> {
                               'Contact us',
                               style: TextStyle(fontSize: title, fontWeight: FontWeight.bold),
                             ),
+                          ),
+                          SizedBox(
+                            height: 15,
                           ),
 
                           Container(
