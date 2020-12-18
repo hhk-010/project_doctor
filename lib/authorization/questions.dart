39787a604b23fab8f230a6af88c03c539f2a3b1b
khagani <ahmedkhagani93@gmail.com>
2020-12-18 08:39:07 +0300

    langChanges

--- lib/authorization/questions.dart
+++ lib/authorization/questions.dart
@@ -43,13 +43,41 @@ class QuestionsShuffle {
   ];
   static final Map choices = {
     '60 years old male with +ve PMH for HTN and DM presented to the ER complaining of central suffocating chest pain, dyspnea and vomiting OE conscious ,alert ,oriented ,looks ill ,BP 190/100,ecg shows St segment elevation in L1,avL,V5,V6 with reciprocal changes and elevated troponin,what is the most likely dx?':
-        ['Stroke', 'Pericarditis', 'Pulseless electrical activity', 'Ventricular fibrillation', 'Acute MI', 'Pectoralis spasm'],
+        [
+      'Stroke',
+      'Pericarditis',
+      'Pulseless electrical activity',
+      'Ventricular fibrillation',
+      'Acute MI',
+      'Pectoralis spasm'
+    ],
     '65 years old male with +ve PMH for HTN,ischemic heart disease with previous CCU admissions,presented with dyspnea ,cough and white colored sputum ,OE he was dyspneic ,BP 100/60 ,chest auscultation reveals bilateral basal crepitations he brought his previous investigations with him and his echocardiogram shows EF=30%, and his current ecg shows old ischemic changes and his current WBC count is normal but his PCV is relatively low ,what is the most likely dx?':
-        ['Lobar pneumonia', 'Asthma', 'Pleural effusion', 'Atrial septal defect', 'Pulmonary embolism', 'Heart failure exacerbation'],
+        [
+      'Lobar pneumonia',
+      'Asthma',
+      'Pleural effusion',
+      'Atrial septal defect',
+      'Pulmonary embolism',
+      'Heart failure exacerbation'
+    ],
     '15 years old female pt known case of type 1 DM developed ,flu like illness and fever followed by polyuria , polydipsia,abdominal pain and vomiting , associated with fruity odor breathing ,presented to the ER , her pulse rate is 120 bpm,temp is 38 C axillary corrected, she looks tachypneic, her RBS is about 500 mg/dl and her ABG shows elevated pH,what is the most likely dx?':
-        ['Tonsillitis', 'Gastroenteritis', 'Diabetes insipidus', 'Hypercalcemia', 'UTI', 'DKA with intercurrent infection'],
+        [
+      'Tonsillitis',
+      'Gastroenteritis',
+      'Diabetes insipidus',
+      'Hypercalcemia',
+      'UTI',
+      'DKA with intercurrent infection'
+    ],
     '6 years old girl with -v PMH presented to the ER with moderate to severe abdominal pain (started few hours before presentation) in the umbilical area and shifted to the Rt iliac fossa , associated with nausea, vomiting , anorexia, fever, OE she looks ill with tachycardia , her abdominal examination shows Rt iliac fossa tenderness and positive rebound tenderness, her WBC count was > 13000,what is the most likely dx?':
-        ['Gastroenteritis', 'Chronic pancreatitis', 'Inguinal hernia', 'Acute appendicitis', 'Abdominal aortic aneurysm', 'Left ureteric stone'],
+        [
+      'Gastroenteritis',
+      'Chronic pancreatitis',
+      'Inguinal hernia',
+      'Acute appendicitis',
+      'Abdominal aortic aneurysm',
+      'Left ureteric stone'
+    ],
     '70 years old male patient with +ve PMH for DM,chronic renal failure on insulin and scheduled hemodialysis presented to the ER with coma which he developed right before presentation his BP was 140/90, PR was normal ,neurological examination shows no focal neurological deficit as a matter of urgency he was given hypertonic solution with rapid regaining of his consciousness level his brain imaging was normal and his biochemistry (apart from low RBS) was acceptable, what is the most likely dx?':
         [
       'Intracranial hemorrhage',
@@ -60,9 +88,23 @@ class QuestionsShuffle {
       'Hypernatremia',
     ],
     '65 years old male heavy smoker (5 packs/day) with -ve PMH presented to the clinic with dyspnea,cough ,sputum,general examination reveals generalized muscle wasting ,chest auscultation reveals decrease air entry , normal vesicular breathing ,wheeze all over the chest,his chest X-ray reveals mediastinal narrowing and flattened diaphragm, what is the most likely dx?':
-        ['Lobar pneumonia', 'COPD', 'Pulmonary embolism', 'Pleural effusion', 'Pericarditis', 'Cardiac tamponade'],
+        [
+      'Lobar pneumonia',
+      'COPD',
+      'Pulmonary embolism',
+      'Pleural effusion',
+      'Pericarditis',
+      'Cardiac tamponade'
+    ],
     '18 years old female presented to the ER with motor vehicle accident,regarding ABC of trauma ,her airway was patent ,she was looking dyspneic,OE she was hypotensive and her PR was 130 BPM ,her chest examination reveals deviation of trachea and apex beat to the left side,and diminished air entry in the right side and audible S1,S2,what is the most likely dx?':
-        ['Foreign body aspiration', 'Pulmonary embolism', 'Tension pneumothorax', 'Cardiac tamponade', 'Asthma exacerbation', 'HYS'],
+        [
+      'Foreign body aspiration',
+      'Pulmonary embolism',
+      'Tension pneumothorax',
+      'Cardiac tamponade',
+      'Asthma exacerbation',
+      'HYS'
+    ],
     '6 years old girl with -ve PMH , healthy wealthy was eating peanut for the first time, she suddenly developed generalized skin redness her parents brought her to the ER on there way her condition had been deteriorated, in the ER  BP was very low ,her PR was elevated and she developed cold extremities , she was given IM adrenaline with good response, what is the most likely dx?':
         [
       'Septic shock',
@@ -73,7 +115,14 @@ class QuestionsShuffle {
       'Neurogenic shock',
     ],
     '34 years old male ,heavy weight lifter with -ve PMH , developed back pain after his exercise,treated by mobic 7.5 mg twice daily with good response after few weeks without discontinuing his treatment,he presented to the ER with abdominal pain and coffee ground vomiting,what is the most likely dx?':
-        ['Acute appendicitis', 'Acute pyelonephritis', 'Peptic ulcer disease', 'Musculoskeletal pain', 'Colorectal CA', 'Chronic pancreatitis'],
+        [
+      'Acute appendicitis',
+      'Acute pyelonephritis',
+      'Peptic ulcer disease',
+      'Musculoskeletal pain',
+      'Colorectal CA',
+      'Chronic pancreatitis'
+    ],
     '58 years old female with history of unresectable CA colon developed colicky abdominal pain and constipation , the pain increased in severity and she also developed abdominal distension and vomiting,in the ER she was dehydrated and her pulse rate was elevated,abdominal auscultation reveals absent bowel sounds , her abdominal plain X-ray shows dilated bowel loops with air fluid level , what is the most likely dx?':
         [
       'Perforated DU',
@@ -93,7 +142,14 @@ class QuestionsShuffle {
       'Gastroenteritis',
     ],
     '52 years old male known case of uncontrolled HTN developed loss of consciousness after domestic stressful event,presented to the ER after few hours of his unconsciousness his BP was 240/120 mm Hg ,unconscious , dysnpneic with clear chest on auscultation , his neurological examination reveals  GCS about 8 and +ve babinski sign in the Rt side,his CT scan reveals white lesion in the left side of the brain and his biochemistry  panel was near normal , what is the most likely dx?':
-        ['Hyponatremia', 'Ischemic stroke', 'Hypoglycemia', 'Uremic encephalopathy', 'Hypercalcemia', 'ICH'],
+        [
+      'Hyponatremia',
+      'Ischemic stroke',
+      'Hypoglycemia',
+      'Uremic encephalopathy',
+      'Hypercalcemia',
+      'ICH'
+    ],
     '21 years old female developed butterfly rash on her face  associated with joint pain in multiple regions ,she also developed ecchymosis in multiple sites her investigations reveals elevated anti nuclear antibody and anti double stranded antibody,what is the most likely dx?':
         [
       'Contact dermatitis',
@@ -113,7 +169,14 @@ class QuestionsShuffle {
       'B6 deficiency',
     ],
     '5 years old male with +ve PMH of X linked coagulation disorder developed massive knee joint swelling after falling on the ground, his joint aspiration reveals fresh blood, what is the most likely dx?':
-        ['Hemolytic anemia', 'Thalassemia', 'Acquired bleeding tendency disorder', 'Hemophilia', 'DVT', 'Septic arthritis'],
+        [
+      'Hemolytic anemia',
+      'Thalassemia',
+      'Acquired bleeding tendency disorder',
+      'Hemophilia',
+      'DVT',
+      'Septic arthritis'
+    ],
     '7 years old male with +ve family history for x-linked RBC disorder developed generalized pallor and dark color urine  after eating fava beans, his examination reveals ,yellowish discoloration of the sclera ,his PCV was 26% , with normal WBC and platelet count his TSB was elevated with normal direct portion, what is the most likely dx?':
         [
       'Hepatitis A',
